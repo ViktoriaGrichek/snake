fb423f4c749436032f8c8ebbf78c0092b13ef1e6
ViktoriaGrichek <viktoria.ellis@mail.ru>
2018-09-16 12:09:26 +0400

    Create window

---
+++ src/com/company/Window.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+    Window() {
+        setVisible(true);
+        setBounds(120, 120, 800, 600);
+        setTitle("Snake Game");
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
